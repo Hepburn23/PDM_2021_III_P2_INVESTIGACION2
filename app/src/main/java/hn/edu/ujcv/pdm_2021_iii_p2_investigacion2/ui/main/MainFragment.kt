ba0df61627a5b9f15ace2443d880cd71d1954685
Hepburn <91041472+Hepburn23@users.noreply.github.com>
2021-10-22 23:32:23 -0600

    probando la app con moneda de Dolares a lempiras

--- app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p2_investigacion2/ui/main/MainFragment.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p2_investigacion2/ui/main/MainFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import hn.edu.ujcv.pdm_2021_iii_p2_investigacion2.R
+import kotlinx.android.synthetic.main.main_fragment.*
 
 class MainFragment : Fragment() {
 
@@ -27,6 +28,35 @@ class MainFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
+        txvresult.text = viewModel.getResults().toString()
+        btnConvert.setOnClickListener {
+            if (txtConvert.text.isNotEmpty()){
+                viewModel.setDollar(txtConvert.text.toString())
+                txvresult.text = viewModel.getResults().toString()
+            }
+            else
+                txvresult.text = "campo requerido"
+        }
+
+        txvresult.text = viewModel.getResults().toString()
+        btnConvert.setOnClickListener {
+            if (txtConvert.text.isNotEmpty()){
+                viewModel.setMexican(txtConvert.text.toString())
+                txvresult.text = viewModel.getResults().toString()
+            }
+            else
+                txvresult.text = "campo requerido"
+        }
+
+        txvresult.text = viewModel.getResults().toString()
+        btnConvert.setOnClickListener {
+            if (txtConvert.text.isNotEmpty()){
+                viewModel.setCanadian(txtConvert.text.toString())
+                txvresult.text = viewModel.getResults().toString()
+            }
+            else
+                txvresult.text = "campo requerido"
+        }
     }
 
 }
--- app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p2_investigacion2/ui/main/MainViewModel.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_iii_p2_investigacion2/ui/main/MainViewModel.kt
@@ -4,4 +4,27 @@ import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private val USD_TO_HNL = 24.18f
+    private var moneyText = ""
+    private val MexicanDollar_TO_HNL = 1.20f
+    private val CanadianD_TO_HNL = 19.55f
+
+    private var result: Float = 0f
+
+    fun setDollar(value: String) {
+        this.moneyText = value
+        result = value.toFloat() * USD_TO_HNL
+    }
+    fun setMexican(value: String) {
+        this.moneyText = value
+        result = value.toFloat() * MexicanDollar_TO_HNL
+    }
+    fun setCanadian(value: String) {
+        this.moneyText = value
+        result = value.toFloat() * CanadianD_TO_HNL
+    }
+    fun getResults(): Float?{
+        return result
+    }
+
 }
